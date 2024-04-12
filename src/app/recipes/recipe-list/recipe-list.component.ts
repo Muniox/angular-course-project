6de3107abdda85d8603a1691e3d9e7c2c781406d
Muniox <muniox20@gmail.com>
2024-04-12 23:13:13 +0200

    Passing Recipe Data with Property Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,5 +14,10 @@ export class RecipeListComponent {
       'This is simply a test',
       'https://glutenfreecuppatea.co.uk/wp-content/uploads/2022/01/gluten-free-smoky-paprika-chicken-recipe-2.jpg'
     ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://glutenfreecuppatea.co.uk/wp-content/uploads/2022/01/gluten-free-smoky-paprika-chicken-recipe-2.jpg'
+    ),
   ];
 }
