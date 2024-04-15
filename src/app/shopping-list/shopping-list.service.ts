34347bb815ef6c394e7fdc78c89c9c9a227469d9
Muniox <muniox20@gmail.com>
2024-04-15 11:24:34 +0200

    Adding the Shopping List Service

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor() {}
+
+  getIngredients(): Ingredient[] {
+    return [...this.ingredients];
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
