0656c127a01e067948e52efb41b4ca0217d23a03
Muniox <muniox20@gmail.com>
2024-04-22 13:36:02 +0200

    Setting Up Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import {DropdownDirective} from "./shared/dropdown.directive";
+import { DropdownDirective } from "./shared/dropdown.directive";
+import { AppRoutingModule } from "./app-routin.module";
+
 
 
 @NgModule({
@@ -25,7 +27,7 @@ import {DropdownDirective} from "./shared/dropdown.directive";
     ShoppingEditComponent,
     DropdownDirective
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
