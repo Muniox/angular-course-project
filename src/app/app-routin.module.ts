e13d98b03369c3268f212be74fd51efc0c57f021
Muniox <muniox20@gmail.com>
2024-04-22 13:52:57 +0200

    Child Routes: Challenge

--- src/app/app-routin.module.ts
+++ src/app/app-routin.module.ts
@@ -6,11 +6,12 @@ import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent },
+  // { path: 'recipes/:id'}
   { path: 'shopping-list', component: ShoppingListComponent },
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes), RouterModule.forChild(appRoutes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
