e9811a6e149166dfe2a7c830b6b747a59dc5ea35
Muniox <muniox20@gmail.com>
2024-06-17 18:34:52 +0200

    uninstall ng-env/builder

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,33 +1,28 @@
-import {HttpClient} from "@angular/common/http";
-import {Injectable} from "@angular/core";
-import {RecipeService} from "../recipes/recipe.service";
-import {Recipe} from "../recipes/recipe.model";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class DataStorageService {
-  URL = import.meta.env.NG_APP_URL;
+  URL =
+    'https://ng-course-recipe-book-784e5-default-rtdb.europe-west1.firebasedatabase.app/';
 
-  constructor(
-    private http: HttpClient,
-    private recipeService: RecipeService
-  ) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    console.log(this.URL)
+    console.log(this.URL);
     const recipes = this.recipeService.getRecipes();
-    this.http.put(this.URL + 'recipes.json', recipes)
-      .subscribe((response) => {
-        console.log(response);
+    this.http.put(this.URL + 'recipes.json', recipes).subscribe((response) => {
+      console.log(response);
     });
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.URL + 'recipes.json')
-      .subscribe((recipes) => {
-      this.recipeService.setRecipes(recipes)
-    })
+    this.http.get<Recipe[]>(this.URL + 'recipes.json').subscribe((recipes) => {
+      this.recipeService.setRecipes(recipes);
+    });
   }
-
 }
