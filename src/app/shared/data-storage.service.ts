d640cb7b2b8de5c0ee9d7c8017c33f02f3fa1d4e
Muniox <muniox20@gmail.com>
2024-06-17 12:53:42 +0200

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,24 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {RecipeService} from "../recipes/recipe.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) { }
+  URL = import.meta.env.NG_APP_URL;
+
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService
+  ) {}
+
+  storeRecipes() {
+    console.log(this.URL)
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.URL + 'recipes.json', recipes)
+      .subscribe((response) => {
+        console.log(response);
+    });
+  }
 }
